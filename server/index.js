6478488a7949225884732bbf0d7d4f0c81e96b31
AkshayWankhade23 <akshaywankhade664@gmail.com>
2024-01-03 20:18:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Register Route
-app.use("/api/auth", authRoutes); /* for register post request use http://localhost:4000/api/auth/register route */
+app.use(
+  "/api/auth",
+  authRoutes
+); /* for register post request use http://localhost:4000/api/auth/register route */
 app.use("/api/job", jobRoutes);
 app.get("/", async (req, res) => {
   res.status(200).json({
@@ -30,15 +33,30 @@ app.get("/", async (req, res) => {
 
 // Error handler middleware
 app.use((err, req, res, next) => {
-    console.log(err.stack); // Log the error stack trace to the console
-    res.status(500).json({ error: "Internal server Error" }); // Send a 500 Internal Server Error response to the client
+  console.log(err.stack); // Log the error stack trace to the console
+  res.status(500).json({ error: "Internal server Error" }); // Send a 500 Internal Server Error response to the client
 });
 
 // Start Server and Connected to MONGODB
 const Port = process.env.PORT || 4000;
-app.listen(Port, () => {
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log(`Server is running on http://localhost:${Port}`))
-    .catch((error) => console.log(error));
-});
+// app.listen(Port, () => {
+//   mongoose
+//     .connect(process.env.MONGODB_URL)
+//     .then(() => console.log(`Server is running on http://localhost:${Port}`))
+//     .catch((error) => console.log(error));
+// });
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(Port, () => {
+      console.log(`Server is running on http://localhost:${Port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process if MongoDB connection fails
+  });
